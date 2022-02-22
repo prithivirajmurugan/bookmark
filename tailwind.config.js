eab96109d589f8acd4ad0882a3583bf22881702a
Prithiviraj Murugan <prithivirajmurugan1996@gmail.com>
2022-02-22 15:06:43 +0530

    final commit for css

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        'bookmark-purple': '#5267DF',
+        'bookmark-red': '#FA5959',
+        'bookmark-blue': '#242A45',
+        'bookmark-grey': '#9194A2',
+        'bookmark-white': '#f7f7f7',
+      },
+    },
+    container: {
+      center: true,
+      padding: '1rem',
+      screen: {
+        lg: '1124px',
+        xl: '1124px',
+        '2xl': '1124px',
+      },
+    },
+  },
+  plugins: [],
+};
